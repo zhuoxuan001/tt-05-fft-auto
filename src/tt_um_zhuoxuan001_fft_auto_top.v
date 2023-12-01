e1483dcf84e71d340011d3c65b2bb98cd021b8ce
Zhuoxuan Li <98688570+zhuoxuan001@users.noreply.github.com>
2023-12-01 08:43:48 +0800

    Update tt_um_zhuoxuan001_fft_auto_top.v

--- src/tt_um_zhuoxuan001_fft_auto_top.v
+++ src/tt_um_zhuoxuan001_fft_auto_top.v
@@ -1,9 +1,7 @@
 module tt_um_zhuoxuan001_fft_auto_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    /* verilator lint_off UNUSEDSIGNAL */
     input  wire [7:0] uio_in,   // IOs: Input path
-    /* verilator lint_on UNUSEDSIGNAL */
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
